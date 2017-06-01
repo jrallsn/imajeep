a76686e3cedb0d66da18ceb55f39c4d8f1522227
James Bannerman <james.bannerman@indexexchange.com>
2017-06-01 15:46:46 -0400

    Adding in initial node sql script

--- server/jeepdb.js
+++ server/jeepdb.js
@@ -1,8 +1,9 @@
-
-
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('jeep.db');
 
 module.exports = {
 	createTeamIfNotExists: function (teamName) {
-
+		db.run("INSERT INTO teams (name) SELECT '" + teamName + "' WHERE NOT EXISTS(SELECT 1 FROM teams WHERE name='" + teamName + "');");
 	}
 };
+
