8b41acb01be25741ae190e20f06963a9d930bf96
John Allison <john.allison@indexexchange.com>
2017-06-01 13:35:36 -0400

    Experimental browser-to-server-to-browser communication

--- server/index.js
+++ server/index.js
@@ -2,10 +2,70 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 3000 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
+function generateId() {
+	len = 4;
+    charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-  ws.send('something');
+    var uid = '';
+    for (var i = 0; i < len; i++) {
+        uid += UidCharacterSets[charSet].charAt(Math.floor(Math.random() * UidCharacterSets[charSet].length));
+    }
+
+    return uid;
+}
+
+var activeRooms = {};
+
+wss.on('connection', function connection(ws) {
+
+	///var roomId = generateId();
+	var type;
+
+	function initRoom(){
+  		var roomId =  generateId();
+  		activeRooms[roomId] = [ws];
+  		ws.send(JSON.stringify({
+  			action: 'roomCreated',
+  			roomId: roomId
+  		}));
+  	}
+
+  	function joinRoom(roomId) {
+  		if (activeRooms[roomId]){
+  			activeRooms[roomId].push(ws);
+  		} else {
+
+  		}
+  	}
+
+  	function testMsg(roomId, content){
+  		if (!activeRooms[roomId]){
+  			return;
+  		}
+
+  		for (var i = 0; i < activeRooms[roomId].length; i++){
+  			var socket = activeRooms[roomId][i];
+  			socket.send(JSON.stringify({
+  				message: msg
+  			}));
+  		}
+  	}
+
+	ws.on('message', function incoming(message) {
+    	console.log('received: %s', message);
+    	var data = JSON.parse(message);
+    	var action = data.action;
+
+    	if(action === 'init'){
+    		initRoom();
+    	}
+    	if(action === 'join') {
+    		joinRoom(data.roomId);
+    	}
+    	if(action === 'testMsg') {
+    		testMsg(data.roomId, data.message);
+    	}
+  	});
+
+  	ws.send('something');
 });
