1d8a9498274f09c9bae6a332787a4271d0e803e4
James Bannerman <james.bannerman@indexexchange.com>
2017-06-01 17:11:48 -0400

    DB functions

--- server/jeepdb.js
+++ server/jeepdb.js
@@ -1,9 +1,34 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('jeep.db');
+var db = new sqlite3.Database('../db/jeep.db');
 
 module.exports = {
 	createTeamIfNotExists: function (teamName) {
-		db.run("INSERT INTO teams (name) SELECT '" + teamName + "' WHERE NOT EXISTS(SELECT 1 FROM teams WHERE name='" + teamName + "');");
+		db.run("INSERT OR IGNORE INTO teams (name) VALUES ('" + teamName + "')");
+	},
+	createSprint: function (sprintName, teamName) {
+		var teamID;
+		db.get("SELECT teamID FROM teams WHERE name='"+teamName + "'", function(err, row) {
+			teamID = row.teamID;
+			db.run ("INSERT OR IGNORE INTO sprints (name, teamID) VALUES('" + sprintName + "', " + teamID + ")");
+		});
+	},
+	getIncompleteActionItems: function(teamName, callback) {
+		var actionItems = [];
+		db.each("SELECT a.* FROM actions as a JOIN teams as t on t.teamID=a.teamID WHERE a.status=0 and t.name='" + teamName +"'", function(err, rows) {
+			actionItems.push(rows);
+		}, function() {
+			callback(actionItems);
+		});
+	},
+	getPreviousSprintInfo: function(teamName, callback) {
+		var previousSprints = [];
+		db.each("SELECT s.* FROM sprints as s JOIN teams as t on t.teamID=s.teamID WHERE t.name='" + teamName +"'", function(err, rows) {
+			previousSprints.push(rows);
+		}, function() {
+			callback(previousSprints);
+		});
+	},
+	actionItemComplete: function(actionID) {
+		db.run("UPDATE actions SET status=1 WHERE actionID=" + actionID);
 	}
 };
-
