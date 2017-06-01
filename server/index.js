f079d52ff10d54d82409eeab0578994390044144
John Allison <john.allison@indexexchange.com>
2017-06-01 17:23:22 -0400

    Complete action items rating flow, in theory

--- server/index.js
+++ server/index.js
@@ -3,13 +3,17 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 3000 });
 var JeepDbAdapter = require('./jeepdb.js');
 
+const votingTimeLimit = 60000;  // default time limit in ms
+const resultsDisplayTime = 10000; // time to display results in ms
+const actionItemCompleteProportion = 0.75;
 
 const GameStates = {
     WAITING_FOR_PLAYERS: 'WAITING_FOR_PLAYERS',
     SELECT_GAME: 'SELECT_GAME',
     FEELS_MENU: 'FEELS_MENU',
     SHOW_OLD_ACTION_ITEMS_LIST: 'SHOW_OLD_ACTION_ITEMS_LIST',
-    REVIEW_AN_ACTION_ITEM: 'REVIEW_AN_ACTION_ITEM',
+    REVIEW_ACTION_ITEMS: 'REVIEW_ACTION_ITEMS',
+    SHOW_ACTION_ITEMS_RESULTS: 'SHOW_ACTION_ITEMS_RESULTS',
     SUBMIT_SADS: 'SUBMIT_SADS',
     DISCUSS_SADS: 'DISCUSS_SADS',
     VOTE_SADS: 'VOTE_SADS',
@@ -32,6 +36,24 @@ function generateId() {
 }
 
 var activeRooms = {};
+
+function updateAllPlayers (roomId, updateObject) {
+    var updateText = JSON.stringify(updateObject);
+    
+    for (var i = 0; i < activeRooms[roomId].players.length; i++){
+        activeRooms[roomId].players[i].socket.send(updateText);
+    }
+}
+
+function updateMasterClient (roomId, updateObject) {
+    var updateText = JSON.stringify(updateObject);
+    activeRooms[roomId].masterSocket.send(updateText);
+}
+
+function updateAllClients (roomId, updateObject) {
+    updateMasterClient (roomId, updateObject);
+    updateAllPlayers (roomId, updateObject);
+}
 
 wss.on('connection', function connection(ws) {
 
@@ -56,23 +78,237 @@ wss.on('connection', function connection(ws) {
   	}
 
   	function joinRoom(roomId, userName) {
-  		if (activeRooms[roomId]){
+  		if (activeRooms[roomId].state === GameStates.WAITING_FOR_PLAYERS){
   			activeRooms[roomId].players.push({
                 playerName: userName,
                 socket: ws
             });
 
-            /* send notification to everyone */
+            var allUserNames = [];
+            
+            for (var i = 0; i < activeRooms[roomId].players.length; i++){
+                allUserNames.push(activeRooms[roomId].players[i].playerName);
+            }
+
+            var gameUpdate = {
+                info: 'userJoined',
+                roomId: roomId,
+                userName: userName,
+                activeUsers: allUserNames,
+                state: activeRooms[roomId].state
+            };
+
+            updateAllClients(gameUpdate);
   		} else {
 
   		}
   	}
 
-  	function testMsg(roomId, content){
-  		if (!activeRooms[roomId]){
-  			return;
-  		}
+    function allPlayersJoined(roomId) {
+        activeRooms[roomId].state = GameStates.SELECT_GAME;
 
+        var gameUpdate = {
+            info: 'allPlayersJoined',
+            roomId: roomId,
+            state: activeRooms[roomId].state
+        };
+
+        updateAllClients(gameUpdate);
+    }
+
+    function selectGame(roomId, gameName) {
+        activeRooms[roomId].state = GameStates.FEELS_MENU;
+
+        var gameUpdate = {
+            info: 'gameSelected',
+            roomId: roomId,
+            state: activeRooms[roomId].state,
+            gameName: gameName
+        };
+    }
+
+    function getPreviousSprintInfo(roomId) {
+        var teamName = activeRooms[roomId].teamName;
+
+        JeepDbAdapter.getPreviousSprintInfo(teamName, function(sprintInfo){
+            var gameUpdate = {
+                info: 'previousSprintInfo',
+                roomId: roomId,
+                state: activeRooms[roomId].state,
+                teamName: teamName,
+                message: sprintInfo
+            };
+
+            activeRooms[roomId].masterSocket.send(JSON.stringify(gameUpdate));
+        });        
+    }
+
+    function newSprint (roomId, sprintName) {
+        activeRooms[roomId].state = GameStates.SHOW_OLD_ACTION_ITEMS_LIST;
+        var teamName = activeRooms[roomId].teamName;
+
+        JeepDbAdapter.getIncompleteActionItems(teamName, function(oldActionItems){
+            var gameUpdate = {
+                info: 'oldActionItems',
+                roomId: roomId,
+                state: activeRooms[roomId].state,
+                teamName: teamName,
+                message: oldActionItems
+            };
+
+            updateAllClients(gameUpdate);
+        });  
+    }
+
+    function startActionItems (roomId) {
+        activeRooms[roomId].state = GameStates.REVIEW_ACTION_ITEMS;
+
+        var oldActionItems = JeepDbAdapter.getIncompleteActionItems(teamName, function(oldActionItems){
+            activeRooms[roomId].actionItemsToRate = oldActionItems;
+            activeRooms[roomId].voteReceived = {};
+            activeRooms[roomId].actionItemsCount = oldActionItems.length;
+            activeRooms[roomId].actionItemsCompleted = 0;
+
+            for(var i = 0; i < activeRooms[roomId].players.length; i++){
+                activeRooms[roomId].voteReceived[activeRooms[roomId].players[i].playerName] = false;
+            }
+
+            startVoting(roomId);
+        });
+    }
+
+    function startVoting (roomId) {
+        if (activeRooms[roomId].actionItemsToRate.length === 0) {
+            endActionItemsPhase(roomId);
+            return;
+        }
+
+        for(var userName in activeRooms[roomId].voteReceived) {
+            if(!activeRooms[roomId].voteReceived.hasOwnProperty(userName)){
+                continue;
+            }
+
+            activeRooms[roomId].voteReceived[userName] = false;
+        }
+        activeRooms[roomId].votes = {};
+
+        activeRooms[roomId].currentActionItem = activeRooms[roomId].actionItemsToRate.pop();
+
+        var gameUpdate = {
+            info: 'startVoting',
+            roomId: roomId,
+            itemInfo: activeRooms[roomId].currentActionItem.description,
+            timeLimit: votingTimeLimit
+        };
+
+        activeRooms[roomId].timer = setTimeout(function(){
+            endVoting(roomId);
+        }, votingTimeLimit);
+
+        updateAllClients(roomId, gameUpdate);
+    }
+
+    function endVoting (roomId) {
+        clearTimeout(activeRooms[roomId].timer);
+
+        var playerUpdate = {
+            info: 'votingEnded',
+            roomId: roomId
+        };
+
+        updateAllPlayers(roomId, playerUpdate);
+
+        var totalVote = 0;
+
+        for(var uname in activeRooms[roomId].votes){
+            if(!activeRooms[roomId].votes.hasOwnProperty(uname)){
+                continue;
+            }
+
+            totalVote += activeRooms[roomId].votes[uname];
+        }
+
+        var votingResultText = totalVote + ' / ' + activeRooms[roomId].players.length;
+        var actionItemComplete = false;
+        if ((totalVote / activeRooms[roomId].players.length) >= actionItemCompleteProportion) {
+            actionItemComplete = true;
+            activeRooms[roomId].actionItemsCompleted++;
+        }
+
+        if(actionItemComplete){
+            JeepDbAdapter.setActionItemComplete(activeRooms[roomId].currentActionItem.actionID);
+        }
+
+        var masterUpdate = {
+            info: 'votingEnded',
+            roomId: roomId,
+            whoVoted: activeRooms[roomId].voteReceived,
+            itemInfo: actionItem.text,
+            voteResult: votingResultText,
+            itemComplete: actionItemComplete,
+            displayTime: resultsDisplayTime
+        };
+
+        updateMasterClient(roomId, masterUpdate);
+
+        setTimeout(startVoting(roomId), resultsDisplayTime);
+    }
+
+    function recordVote (roomId, userName, vote) {
+        if (activeRooms[roomId].voteReceived[userName]){
+            // don't vote twice
+            return;
+        }
+
+        activeRooms[roomId].voteReceived[userName] = true;
+        activeRooms[roomId].votes[userName] = vote;
+
+        var allVotesReceived = true;
+
+        for(var user in activeRooms[roomId].voteReceived) {
+            if(!activeRooms[roomId].voteReceived.hasOwnProperty(user)){
+                continue;
+            }
+
+            if(!activeRooms[roomId].voteReceived[user]){
+                allVotesReceived = false;
+                break;
+            }
+        }
+
+        if(allVotesReceived) {
+            endVoting (roomId);
+        }
+    }
+
+    function endActionItemsPhase (roomId) {
+        delete activeRooms[roomId].voteReceived;
+        delete activeRooms[roomId].votes;
+
+        activeRooms[roomId].state = GameStates.SHOW_ACTION_ITEMS_RESULTS;
+
+        var masterUpdate = {
+            info: 'actionItemsResults',
+            roomId: roomId,
+            state: activeRooms[roomId].state,
+            actionItemsCompletedNumber: activeRooms[roomId].actionItemsCompleted,
+            actionItemsTotalNumber: activeRooms[roomId].actionItemsCount,
+            displayTime: resultsDisplayTime
+        };
+
+        delete activeRooms[roomId].actionItemsCompleted;
+        delete activeRooms[roomId].actionItemsCount;
+
+        updateMasterClient(roomId, masterUpdate);
+
+        setTimeout(startFeels(roomId), resultsDisplayTime);
+    }
+
+    function startFeels(roomId) {
+
+    }
+
+  	function testMsg(roomId, content){
   		for (var i = 0; i < activeRooms[roomId].length; i++){
   			var socket = activeRooms[roomId][i];
   			socket.send(JSON.stringify({
@@ -91,6 +327,10 @@ wss.on('connection', function connection(ws) {
             return;
         }
     	var action = data.action;
+        if(data.roomId && !activeRooms[data.roomId]){
+            console.error('Attempted action ' + data.action + ' but Room ID ' + roomId + ' not initialized');
+            return;
+        }
 
         switch (action) {
             case 'init':
@@ -103,18 +343,22 @@ wss.on('connection', function connection(ws) {
                 testMsg(data.roomId, data.message);
                 break;
             case 'allPlayersJoined':
-                /* TODO */
+                allPlayersJoined(data.roomId);
                 break;
             case 'selectGame':
-                /* TODO */
+                selectGame(data.roomId, data.gameName);
                 break;
             case 'previousSprints':
+                getPreviousSprintInfo(data.roomId);
                 break;
             case 'newSprint':
+                newSprint(data.roomId, data.sprintName);
                 break;
             case 'startActionItems':
+                startActionItems(data.roomId);
                 break;
-            case 'submitVotes':
+            case 'submitVote':
+                recordVote(data.roomId, data.userName, data.vote);
                 break;
             case 'submitFeedback':
                 break;
